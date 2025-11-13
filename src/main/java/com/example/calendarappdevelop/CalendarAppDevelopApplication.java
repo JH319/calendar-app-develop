078b8ba3d53eba0736312fffb957b03a81c567c2
Jeongha Im <yimjh3205@gmail.com>
2025-11-13 16:45:31 +0900

    Initial commit

---
+++ src/main/java/com/example/calendarappdevelop/CalendarAppDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.calendarappdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarAppDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarAppDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calendarappdevelop/CalendarAppDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calendarappdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarAppDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
